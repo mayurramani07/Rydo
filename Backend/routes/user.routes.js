87c7ef4e21c66b52af926dd036df5b38626b4f01
Mayur Ramani <ramanimayur836@gmail.com>
2025-06-21 16:18:26 +0530

    User Login Completed

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -19,4 +19,11 @@ router.post('/register', [
     userController.registerUser
 );
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min :  6}).withMessage('Password')
+],
+    userController.loginuser
+)
+
 module.exports = router;
