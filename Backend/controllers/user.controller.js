d74e62f79c9a2fb981f8f1632fe8cb4fe5f70e83
Mayur Ramani <ramanimayur836@gmail.com>
2025-06-29 18:41:28 +0530

    Captain Registration Completed

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,6 +11,12 @@ module.exports.registerUser = async (req, res, next) => {
 
     const { fullname, email, password } = req.body;
 
+    const isUserAlready = await userModel.findOne({ email });
+
+    if(isUserAlready) {
+        return res.status(400).json({message : 'User already exist'});
+    }
+
     try {
         const hashPassword = await userModel.hashPassword(password);
 
